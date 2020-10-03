09f38e39725f24703810e6167f226cc33c7c0f0d
Kirill Spitsyn <ksp@google.com>
2020-10-03 14:48:38 -0700

    Add some quoting to host's bashrc.

--- make_host.sh
+++ make_host.sh
@@ -101,7 +101,7 @@ END
   sudo sh -c "cat >| $mnt_dir/root/.bashrc" <<'END'
 # https://unix.stackexchange.com/questions/16578/resizable-serial-console-window/283206#283206
 res() {
-  old=$(stty -g)
+  old="$(stty -g)"
   stty raw -echo min 0 time 5
   printf '\0337\033[r\033[999;999H\033[6n\0338' > /dev/tty
   IFS='[;R' read -r _ rows cols _ < /dev/tty
@@ -110,7 +110,7 @@ res() {
   # echo "rows:$rows"
   stty cols "$cols" rows "$rows"
 }
-[ $(tty) = /dev/ttyS0 ] && res
+[ "$(tty)" = /dev/ttyS0 ] && res
 
 export EDITOR=vi
 export TERM=screen-256color
