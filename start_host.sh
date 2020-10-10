35f751d6407201e0d946d2c1a0201f96c432a03e
Kirill Spitsyn <ksp@google.com>
2020-10-10 08:13:20 -0700

    Add an option to capture and show all traffic on the interface connecting the VMs.

--- start_host.sh
+++ start_host.sh
@@ -4,6 +4,7 @@ source gbash.sh || exit
 
 DEFINE_dir --required name '' 'Host directory.'
 DEFINE_bool debug false 'Attach to the kernel with GDB.'
+DEFINE_bool net_dump false 'Capture traffic on the NIC connecting the VMs.'
 
 gbash::init_google "$@"
 
@@ -14,7 +15,8 @@ root_image="$FLAGS_name/root.img"
 opts=(
   -drive file="$root_image",index=0,format=raw
   -nic user
-  -nic socket,mcast=230.0.0.1:1234
+  -netdev socket,id=eth1,mcast=230.0.0.1:1234
+  -device e1000,netdev=eth1
   -serial mon:stdio
   -display none
   -m 4G
@@ -27,6 +29,15 @@ if (( FLAGS_debug )); then
   # Make sure we are able to start qemu with sudo, before starting gdb.
   sudo true
   tmux new-window -a -d -n "gdb $FLAGS_name" "cd kernels/linux; gdb vmlinux -ex 'target remote localhost:1234' -ex continue"
+fi
+
+if (( FLAGS_net_dump )); then
+  pcap_path="$FLAGS_name/net_dump.pcap"
+  opts+=(
+    -object filter-dump,id=net_dump_1,netdev=eth1,file="$pcap_path"
+  )
+  rm -f "$pcap_path"
+  tmux new-window -a -d -n "net-dump $FLAGS_name" "tail -n 10000 -F $pcap_path | tshark -r- -n --color; sleep 3000"
 fi
 
 sudo qemu-system-x86_64 "${opts[@]}"
